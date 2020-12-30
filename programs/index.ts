7df832752dd95ec7a421be5c13c59f3821325b4d
何飞 <reechand@cust.edu.cn>
2020-12-30 09:07:01 +0800

    记录部署的一些配置

---
+++ programs/index.ts
@@ -0,0 +1,5 @@
+#! /usr/bin/node
+
+if (module===require.main){
+    console.log("主程序测试");
+} 
---
+++ 同步脚本/配置.远端.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+ 
+# 远端信息
+RemoteHost=192.168.138.7
+RemotePath=/home/hf/docker-compose/quan/programs/
+#RemotePath=/root/hf/docker/tomcat/webapps/ROOT/md/
+SshUserName=hf
+#SshUserName=root
