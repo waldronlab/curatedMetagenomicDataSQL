83df97d0db7f0a24b28e2d3e7f4c460b5afaa4a9
何飞 <reechand@cust.edu.cn>
2021-01-18 15:43:02 +0800

    最简单的http服务程序

--- programs/index.js
+++ programs/index.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = require("./log/logger");
+const http = require("http");
+const util = require("util");
+const url = require("url");
 if (module === require.main) {
     logger_1.log.info("xxxxxxxxxxxxxxxxxxxxxxxx");
     logger_1.log.fatal("fffffffffffffff");
     console.log("主程序测试");
+    http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(util.inspect(url.parse(req.url, true)));
+    }).listen(3000);
 }
 //# sourceMappingURL=index.js.map
--- programs/index.ts
+++ programs/index.ts
@@ -1,7 +1,14 @@
 import {log} from "./log/logger";
+const http=require("http");
+const util=require("util");
+const url=require("url");
 
 if (module===require.main){
     log.info("xxxxxxxxxxxxxxxxxxxxxxxx");
-    log.fatal("fffffffffffffff")
+    log.fatal("fffffffffffffff");
     console.log("主程序测试");
+    http.createServer(function(req, res){
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end(util.inspect(url.parse(req.url, true)));
+    }).listen(3000);
 }
