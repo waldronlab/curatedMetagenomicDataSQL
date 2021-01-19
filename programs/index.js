11274056c7b17b5f7184c3e28df4b3fda03dbc4f
何飞 <reechand@cust.edu.cn>
2021-01-19 21:01:12 +0800

    express程序

--- programs/index.js
+++ programs/index.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const util = require("util");
 const url = require("url");
+const express = require("express");
+const app = express();
+app.use('/', function (req, res) {
+    res.send('root path');
+});
 if (module === require.main) {
-    http.createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(util.inspect(url.parse(req.url, true)));
-    }).listen(3000);
-    console.log("最简单的http服务程序");
+    http.createServer(app).listen(3000);
+    console.log("express的http服务程序");
 }
 //# sourceMappingURL=index.js.map
