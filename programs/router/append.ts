50bb247277d9845e11d8c4c77c5a96b5a1c0679b
何飞 <reechand@cust.edu.cn>
2021-02-25 12:21:34 +0800

    开始实施样本追加

---
+++ programs/router/append.ts
@@ -0,0 +1,31 @@
+//第一部分：所有路由都相同的头部
+import express=require("express");
+const router=express.Router();
+import {tableBaseV3} from "../sampleCenter/tableBaseV3";
+
+/**
+ * @swagger
+ * /append/new:
+ *   post:
+ *     tags:
+ *       - append
+ *     summary: directly get last sample without input
+ *     description: get last inserted sample
+ *     responses:
+ *       "200":
+ *         description: success ，last inserted sample。
+ */
+router.post('/new', async function (req, res) {
+    let obj: tableBaseV3 = new tableBaseV3();
+    obj.tableName = "samples";
+    obj.schema = "v1";
+    var objJson:any=await obj.last();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    objJson=JSON.stringify(objJson.data);
+    res.end(objJson);
+});
+
+
+
+//最后部分：是把路由暴露给其它模块的代码
+module.exports=router;
