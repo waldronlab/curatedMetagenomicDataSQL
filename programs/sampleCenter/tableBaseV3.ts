24726e4a653da865251ef094ff876688f2e96b08
何飞 <reechand@cust.edu.cn>
2021-03-01 20:10:29 +0800

    追加样本的注释错误，现在已得到修正。

--- programs/sampleCenter/tableBaseV3.ts
+++ programs/sampleCenter/tableBaseV3.ts
@@ -2,7 +2,7 @@ import {tableBaseV2} from "./tableBaseV2";
 import {tableBaseV1} from "./tableBaseV1";
 import {DataTypes} from "sequelize";
 export class tableBaseV3 extends tableBaseV2 {
-    //返回字段列表:这个方法很重要，决定了创建出来的表到底有哪些列
+    //返回最近新加的样本
     public async last() {
         let dt=await this.defineTable();
         const result = await dt.findOne(
@@ -14,4 +14,5 @@ export class tableBaseV3 extends tableBaseV2 {
         );
         return result;
     }
+    //追加
 }
