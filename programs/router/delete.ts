f9ed6f72016d4f9135485ee592c4616c4a3d46c5
何飞 <reechand@cust.edu.cn>
2021-03-15 20:10:18 +0800

    删除最新添加的样本这个功能成功添加进去了。

--- programs/router/delete.ts
+++ programs/router/delete.ts
@@ -39,6 +39,40 @@ router.post('/where', async function (req, res) {
 });
 
 
+/**
+ * @swagger
+ * /delete/last:
+ *   get:
+ *     tags:
+ *       - delete
+ *     summary: directly delete last sample without input
+ *     produces:
+ *     - application/json
+ *     description: get last inserted sample
+ *     responses:
+ *       "200":
+ *         description: 200 delete successfully
+ *       "404":
+ *         description: not found last sample, number of samples is zero。
+ */
+router.get('/last', async function (req, res) {
+    let obj: tableBaseV3 = new tableBaseV3();
+    obj.tableName = "samples";
+    obj.schema = "v1";
+    var objJson:any=await obj.last();
+
+    if (objJson==null)
+    {
+        res.status(404)
+        res.send("not found!")
+    }
+    else{
+        await obj.deleteSamples({"sampleID":objJson["sampleID"]})
+        res.status(200)
+        res.send("delete successfully!")
+    }
+});
 
 //最后部分：是把路由暴露给其它模块的代码
+
 module.exports=router;
